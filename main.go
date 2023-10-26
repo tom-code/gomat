a6e5aaf3aab945196ed203af0fbabfe53778521c
Tomas Petrilak <tomas.petrilak@mavenir.com>
2023-10-26 08:03:20 +0200

    update

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"crypto/aes"
 	"crypto/ecdh"
 	"crypto/ecdsa"
 	"crypto/rand"
 	"crypto/x509"
-	"encoding/binary"
 	"fmt"
 	"gomat/tlvdec"
 	"log"
@@ -17,160 +14,6 @@ import (
 
 	"github.com/spf13/cobra"
 )
-
-
-func make_nonce3(counter uint32, node []byte) []byte{
-	var n bytes.Buffer
-	n.WriteByte(0)
-	binary.Write(&n, binary.LittleEndian, counter)
-	n.Write(node)
-	return n.Bytes()
-}
-
-
-type Channel struct {
-	udp net.PacketConn
-	remote_address net.UDPAddr
-	out_counter uint32
-}
-
-func NewChannel(remote_ip net.IP, remote_port, local_port int) Channel {
-	var out Channel
-	out.remote_address = net.UDPAddr{
-		IP : remote_ip,
-		Port: remote_port,
-	}
-	var err error
-	out.udp, err = net.ListenPacket("udp", fmt.Sprintf(":%d", local_port))
-	if err != nil {
-		panic(err)
-	}
-	out.out_counter = 1
-	return out
-}
-
-func (ch *Channel)send(data []byte) {
-	ch.udp.WriteTo(data, &ch.remote_address)
-}
-func (ch *Channel)receive() ([]byte, error) {
-	buf := make([]byte, 1024)
-	n, _, errx := ch.udp.ReadFrom(buf)
-	if errx != nil {
-		panic(errx)
-	}
-	return buf[:n], nil
-}
-
-
-type SecureChannel struct {
-	udp *Channel
-	encrypt_key []byte
-	decrypt_key []byte
-	remote_node []byte
-	local_node []byte
-	counter uint32
-	session int
-}
-
-func (sc *SecureChannel) receive() DecodedGeneric {
-	data, _ := sc.udp.receive()
-	decode_buffer := bytes.NewBuffer(data)
-	var out DecodedGeneric
-	out.msg.decodeBase(decode_buffer)
-	add := data[:len(data)-decode_buffer.Len()]
-	proto := decode_buffer.Bytes()
-
-
-	if len(sc.decrypt_key) > 0 {
-		nonce := make_nonce3(out.msg.messageCounter, sc.remote_node)
-		c, err := aes.NewCipher(sc.decrypt_key)
-		if err != nil {
-			panic(err)
-		}
-		ccm, err := NewCCMWithNonceAndTagSizes(c, len(nonce), 16)
-		if err != nil {
-			panic(err)
-		}
-		ciphertext := proto
-		decbuf := []byte{}
-		outx, err := ccm.Open(decbuf, nonce, ciphertext, add)
-		if err != nil {
-			panic(err)
-		}
-
-		decoder := bytes.NewBuffer(outx)
-
-		out.proto.decode(decoder)
-		if len(decoder.Bytes()) > 0 {
-			tlvdata := make([]byte, decoder.Len())
-			n, _ := decoder.Read(tlvdata)
-			out.payload = tlvdata[:n]
-		}
-	} else {
-		out.proto.decode(decode_buffer)
-		if len(decode_buffer.Bytes()) > 0 {
-			tlvdata := make([]byte, decode_buffer.Len())
-			n, _ := decode_buffer.Read(tlvdata)
-			out.payload = tlvdata[:n]
-		}
-	}
-
-	if out.proto.protocolId == 0 {
-		if out.proto.opcode == 0x10 {  // standalone ack
-			return sc.receive()
-		}
-	}
-
-	ack := AckGen(out.proto, out.msg.messageCounter)
-	sc.send(ack)
-
-	if out.proto.protocolId == 0 {
-		if out.proto.opcode == 0x40 {  // status report
-			return out
-		}
-	}
-	if len(out.payload) > 0 {
-		out.tlv = tlvdec.Decode(out.payload)
-	}
-	return out
-}
-
-func (sc *SecureChannel)send(data []byte) {
-
-	sc.counter = sc.counter + 1
-	var buffer bytes.Buffer
-	msg := Message {
-		sessionId: uint16(sc.session),
-		securityFlags: 0,
-		messageCounter: sc.counter,
-		sourceNodeId: []byte{1,2,3,4,5,6,7,8},
-	}
-	msg.encodeBase(&buffer)
-	if len(sc.encrypt_key) == 0 {
-		buffer.Write(data)
-	} else {
-
-		header_slice := buffer.Bytes()
-		add2 := make([]byte, len(header_slice))
-		copy(add2, header_slice)
-
-		nonce := make_nonce3(sc.counter, sc.local_node)
-
-		c, err := aes.NewCipher(sc.encrypt_key)
-		if err != nil {
-			panic(err)
-		}
-		ccm, err := NewCCMWithNonceAndTagSizes(c, len(nonce), 16)
-		if err != nil {
-			panic(err)
-		}
-		CipherText := ccm.Seal(nil, nonce, data, add2)
-		buffer.Write(CipherText)
-	}
-
-
-	sc.udp.send(buffer.Bytes())
-}
 
 
 func do_spake2p(pin int, udp *Channel) SecureChannel {
